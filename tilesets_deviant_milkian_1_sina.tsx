0f2b564da8db44e014a3f89e232de2c8f514cc48
Sina-beepglitch <75430401+Sina-beepglitch@users.noreply.github.com>
2020-12-07 16:29:16 +0100

    design

---
+++ tilesets_deviant_milkian_1_sina.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tilesets_deviant_milkian_1_sina" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../Dropbox (UCS)/UCS-Kundenprojekte/Nürnberg Digital Festival/tilesets_deviant_milkian_1_sina.png" width="512" height="512"/>
+</tileset>
