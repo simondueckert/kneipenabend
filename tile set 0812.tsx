efdb2bfbd822347390d2f6fd0417cd00ba5b848d
Simon Dückert <simon.dueckert@cogneon.de>
2020-12-09 09:11:15 +0100

    Tileset paths fixed

--- tile set 0812.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tile set 0812" tilewidth="32" tileheight="32" tilecount="144" columns="12">
- <image source="../../../../../Volumes/GoogleDrive/Geteilte Ablagen/UCS New/UCS Clients/Spirit Link/2020 Weihnachtsfeier Workadventu_re/Tilesets_fertig/tile set 0812.png" width="384" height="384"/>
-</tileset>
--- tile set Boden 0712.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tile set Boden 0712" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="../../../../../Volumes/GoogleDrive/Geteilte Ablagen/UCS New/UCS Clients/Spirit Link/2020 Weihnachtsfeier Workadventu_re/Tilesets_fertig/tile set Boden 0712.png" width="320" height="320"/>
-</tileset>
